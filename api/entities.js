82823e7e2b4bc3b9a58d37d2f017c74b3be66481
defynepay-dye <defynepay@gmail.com>
2025-11-03 12:41:50 -0600

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,173 @@
+import { base44 } from './base44Client';
+
+
+export const Transaction = base44.entities.Transaction;
+
+export const Pump = base44.entities.Pump;
+
+export const Product = base44.entities.Product;
+
+export const Shift = base44.entities.Shift;
+
+export const InventoryItem = base44.entities.InventoryItem;
+
+export const POSTransaction = base44.entities.POSTransaction;
+
+export const LotteryTicket = base44.entities.LotteryTicket;
+
+export const AgeVerification = base44.entities.AgeVerification;
+
+export const CustomItem = base44.entities.CustomItem;
+
+export const PricingSettings = base44.entities.PricingSettings;
+
+export const TaxSettings = base44.entities.TaxSettings;
+
+export const CustomerDisplay = base44.entities.CustomerDisplay;
+
+export const MixMatchPromotion = base44.entities.MixMatchPromotion;
+
+export const LoyaltyProgram = base44.entities.LoyaltyProgram;
+
+export const ScanDataReport = base44.entities.ScanDataReport;
+
+export const InventoryReceiving = base44.entities.InventoryReceiving;
+
+export const Location = base44.entities.Location;
+
+export const InventoryTransfer = base44.entities.InventoryTransfer;
+
+export const Region = base44.entities.Region;
+
+export const TenderConfiguration = base44.entities.TenderConfiguration;
+
+export const CashManagement = base44.entities.CashManagement;
+
+export const TimeClockEntry = base44.entities.TimeClockEntry;
+
+export const AIInventoryRecommendation = base44.entities.AIInventoryRecommendation;
+
+export const InvoiceAudit = base44.entities.InvoiceAudit;
+
+export const QSRMenu = base44.entities.QSRMenu;
+
+export const FuelReceiving = base44.entities.FuelReceiving;
+
+export const NoSaleTransaction = base44.entities.NoSaleTransaction;
+
+export const CurrencyRate = base44.entities.CurrencyRate;
+
+export const POSTerminal = base44.entities.POSTerminal;
+
+export const CustomerProfile = base44.entities.CustomerProfile;
+
+export const MobileFuelingSession = base44.entities.MobileFuelingSession;
+
+export const AIBusinessIntelligence = base44.entities.AIBusinessIntelligence;
+
+export const SystemNotification = base44.entities.SystemNotification;
+
+export const SupportTicket = base44.entities.SupportTicket;
+
+export const TrainingModule = base44.entities.TrainingModule;
+
+export const Supplier = base44.entities.Supplier;
+
+export const ProductFamily = base44.entities.ProductFamily;
+
+export const SupplierProduct = base44.entities.SupplierProduct;
+
+export const AIFuelPriceRecommendation = base44.entities.AIFuelPriceRecommendation;
+
+export const FuelTank = base44.entities.FuelTank;
+
+export const FuelReconciliation = base44.entities.FuelReconciliation;
+
+export const PurchaseOrder = base44.entities.PurchaseOrder;
+
+export const ModifierGroup = base44.entities.ModifierGroup;
+
+export const NetworkDevice = base44.entities.NetworkDevice;
+
+export const NetworkHealthLog = base44.entities.NetworkHealthLog;
+
+export const WeatherData = base44.entities.WeatherData;
+
+export const SecurityCamera = base44.entities.SecurityCamera;
+
+export const SecurityIncident = base44.entities.SecurityIncident;
+
+export const ShelfMonitoringEvent = base44.entities.ShelfMonitoringEvent;
+
+export const MaintenanceAlert = base44.entities.MaintenanceAlert;
+
+export const EquipmentHealthLog = base44.entities.EquipmentHealthLog;
+
+export const DynamicPricing = base44.entities.DynamicPricing;
+
+export const AccountsPayable = base44.entities.AccountsPayable;
+
+export const Department = base44.entities.Department;
+
+export const StoreLayoutZone = base44.entities.StoreLayoutZone;
+
+export const RestockingTask = base44.entities.RestockingTask;
+
+export const SalesRestrictionRule = base44.entities.SalesRestrictionRule;
+
+export const CategoryTaxRule = base44.entities.CategoryTaxRule;
+
+export const Category = base44.entities.Category;
+
+export const CategoryItem = base44.entities.CategoryItem;
+
+export const MediaAsset = base44.entities.MediaAsset;
+
+export const MediaFolder = base44.entities.MediaFolder;
+
+export const MediaTemplate = base44.entities.MediaTemplate;
+
+export const AdvancedEngagementSettings = base44.entities.AdvancedEngagementSettings;
+
+export const TobaccoScanData = base44.entities.TobaccoScanData;
+
+export const CPGScanData = base44.entities.CPGScanData;
+
+export const AlcoholScanData = base44.entities.AlcoholScanData;
+
+export const TobaccoLoyaltyMember = base44.entities.TobaccoLoyaltyMember;
+
+export const PersonalizedOffer = base44.entities.PersonalizedOffer;
+
+export const ScanDataSubscription = base44.entities.ScanDataSubscription;
+
+export const DeliveryOrder = base44.entities.DeliveryOrder;
+
+export const DeliveryPlatformIntegration = base44.entities.DeliveryPlatformIntegration;
+
+export const ExternalMediaChannel = base44.entities.ExternalMediaChannel;
+
+export const ArmorsafeDeposit = base44.entities.ArmorsafeDeposit;
+
+export const PhysicalCountSession = base44.entities.PhysicalCountSession;
+
+export const CustomerCart = base44.entities.CustomerCart;
+
+export const EmployeePayable = base44.entities.EmployeePayable;
+
+export const ProductMaster = base44.entities.ProductMaster;
+
+export const CancellationReason = base44.entities.CancellationReason;
+
+export const ClientAccount = base44.entities.ClientAccount;
+
+export const LSSConfiguration = base44.entities.LSSConfiguration;
+
+export const HardwareProfile = base44.entities.HardwareProfile;
+
+export const ProvisioningCode = base44.entities.ProvisioningCode;
+
+
+
+// auth sdk:
+export const User = base44.auth;
